591ce4128e72b54e38bd74fd6972c4f74139758e
jakobpalmborg <jakob.palmborg@gmail.com>
2022-10-18 08:13:23 +0200

    add index.html and script.js

---
+++ script.js
@@ -0,0 +1,87 @@
+document.querySelector("form").addEventListener("submit", handleSubmitForm);
+document.querySelector("ul").addEventListener("click", handleClickDeleteOrCheck);
+document.querySelector(".clearAll").addEventListener("click", clearAll);
+
+const todoArr = [];
+let num = 0;
+const numberDone = document.querySelector(".numberDone");
+
+function handleSubmitForm(e) {
+  e.preventDefault();
+  let input = document.querySelector("input");
+  if (input.value != "") addTodo(input.value);
+  input.value = "";
+}
+
+function handleClickDeleteOrCheck(e) {
+  if (e.target.name == "checkButton") checkTodo(e);
+
+  if (e.target.name == "deleteButton") deleteTodo(e);
+}
+
+function addTodo(todo) {
+  let ul = document.querySelector("ul");
+  let li = document.createElement("li");
+  li.innerHTML = `
+        <span class="todo-item">${todo}</span>
+        <button name="checkButton" class="checkBtn">&#10003;</button>
+        <button name="deleteButton" class="deleteBtn">&#10005;</button>
+    `;
+  ul.appendChild(li);
+
+  const todoObject = {};
+  todoObject.todo = todo;
+  todoArr.push(todoObject);
+}
+
+function checkTodo(e) {
+  let item = e.target.parentNode;
+  if (item.className == "") {
+    item.className = "line";
+    addNumberOfTask();
+  } else {
+    item.className = "";
+    takeAwayTask();
+  }
+}
+
+function addNumberOfTask() {
+  num++;
+  numberDone.innerHTML = num;
+}
+
+function takeAwayTask() {
+  num--;
+  numberDone.innerHTML = num;
+}
+
+function deleteTodo(e) {
+  let item = e.target.parentNode;
+  item.remove();
+}
+
+function clearAll() {
+    num = 0;
+    numberDone.innerHTML = num;
+    let ul = document.querySelector(".todo-list");
+    ul.innerHTML = "";
+}
+
+
+
+
+
+
+//Old function
+// function checkTodo(e) {
+//   let item = e.target.parentNode;
+//   if (item.style.textDecoration == "line-through") {
+//     item.style.textDecoration = "none";
+//     takeAwayTask();
+//   } else {
+//     item.style.textDecoration = "line-through";
+//     addNumberOfTask();
+//   }
+// }
+
+
